b1a44fe03f259013ff55b42dc3f81f6a0ee1b427
GrzegorzJendernal <g.jendernal@gmail.com>
2023-10-04 21:06:04 +0200

    added id and orientation to ships

--- src/utils/ships.ts
+++ src/utils/ships.ts
@@ -1,11 +1,41 @@
 interface Ship {
   name: string;
+  id: number;
   length: number;
   hit: boolean;
   sunk: boolean;
+  orientation: "horizontal" | "vertical";
 }
 
-export const singleMastedShip: Ship = {name: "single-masted ship", length: 1, hit: false, sunk: false};
-export const twoMastedShip: Ship = {name: "two-masted ship", length: 2, hit: false, sunk: false};
-export const threeMastedShip: Ship = {name: "three-masted ship", length: 3, hit: false, sunk: false};
-export const fourMastedShip: Ship = {name: "four-masted ship", length: 4, hit: false, sunk: false};
+export const singleMastedShip: Ship = {
+  name: "single-masted ship",
+  id: 1,
+  length: 1,
+  hit: false,
+  sunk: false,
+  orientation: "horizontal",
+};
+export const twoMastedShip: Ship = {
+  name: "two-masted ship",
+  id: 1,
+  length: 2,
+  hit: false,
+  sunk: false,
+  orientation: "horizontal",
+};
+export const threeMastedShip: Ship = {
+  name: "three-masted ship",
+  id: 1,
+  length: 3,
+  hit: false,
+  sunk: false,
+  orientation: "horizontal",
+};
+export const fourMastedShip: Ship = {
+  name: "four-masted ship",
+  id: 1,
+  length: 4,
+  hit: false,
+  sunk: false,
+  orientation: "horizontal",
+};
