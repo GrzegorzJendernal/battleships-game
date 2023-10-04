58b5db530b0c1063391a4dd90740e0d46a3f349b
GrzegorzJendernal <g.jendernal@gmail.com>
2023-10-04 21:57:59 +0200

    actualised Board

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BoardContainer } from "./board.styled";
 import Cell from "./Cell/Cell";
 import { columns, rows } from "../../utils/labels";
 
-const Board = () => {
+function createEmptyBoard() {
+  return rows.map(() => columns.map(() => undefined));
+}
 
+
+const Board = () => {
+  const [boardState, setBoardState] = useState(createEmptyBoard());
+
+  useEffect(() => {
+    const placeShipsRandomly = () => {
+      // Tworzymy nową planszę w oparciu o aktualny stan
+      const newBoard = createEmptyBoard();
+
+      // Umieszczamy statki na nowej planszy
+      // Tutaj możesz dostosować logikę rozmieszczania statków
+
+      setBoardState(newBoard);
+    };
+
+    // Wywołaj funkcję rozmieszczania statków na planszy
+    placeShipsRandomly();
+  }, []);
 
   return (
     <BoardContainer>
@@ -14,15 +34,17 @@ const Board = () => {
         <Cell key={columnIndex}>{column}</Cell>
       ))}
       {/* Plansza gry */}
-      {rows.map((row, rowIndex) => (
+      {boardState.map((row, rowIndex) => (
         <React.Fragment key={rowIndex}>
-          <Cell>{row}</Cell>
-          {columns.map((column, columnIndex) => (
+          <Cell>{rows[rowIndex]}</Cell>
+          {row.map((cell, columnIndex) => (
             <Cell
               key={columnIndex}
-              id={`${column}${row}`}
+              id={`${columns[columnIndex]}${rows[rowIndex]}`}
               coordinate={+`${columnIndex}${rowIndex}`}
-            >{`${+columnIndex}${+rowIndex}`}</Cell>
+            >
+              {cell}
+            </Cell>
           ))}
         </React.Fragment>
       ))}
