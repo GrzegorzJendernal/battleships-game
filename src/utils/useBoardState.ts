81c147a222820a66b0759655a846a86a0f4c69e3
GrzegorzJendernal <g.jendernal@gmail.com>
2023-10-05 22:58:51 +0200

    fixed hook to avoid infinite loop

--- src/utils/useBoardState.ts
+++ src/utils/useBoardState.ts
@@ -5,43 +5,44 @@ import { columns, rows } from "./labels";
 
 export const useBoardState = () => {
   const [boardState, setBoardState] = useState(createEmptyBoard());
+  const [isInitialized, setIsInitialized] = useState(false);
   const getRandomCoordinate = (max: number) => {
     return Math.floor(Math.random() * max);
   };
-  
 
-  useEffect(() => {
-    if (boardState.flat().every((cell) => cell === undefined)) {
-      const placeShipsRandomly = () => {
-        const newBoard = createEmptyBoard();
+  const placeShipsRandomly = () => {
+    const newBoard = createEmptyBoard();
 
-        for (const ship of ships) {
-          let isPlaced = false;
-          while (!isPlaced) {
-            const startRow = getRandomCoordinate(rows.length);
-            const startCol = getRandomCoordinate(columns.length);
-            const orientation = Math.random() < 0.5 ? "horizontal" : "vertical";
+    for (const ship of ships) {
+      let isPlaced = false;
+      while (!isPlaced) {
+        const startRow = getRandomCoordinate(rows.length);
+        const startCol = getRandomCoordinate(columns.length);
+        const orientation = Math.random() < 0.5 ? "horizontal" : "vertical";
 
-            if (canPlaceShip(newBoard, ship, startRow, startCol, orientation)) {
-              isPlaced = true;
+        if (canPlaceShip(newBoard, ship, startRow, startCol, orientation)) {
+          isPlaced = true;
 
-              for (let i = 0; i < ship.length; i++) {
-                if (orientation === "horizontal") {
-                  newBoard[startRow][startCol + i] = ship;
-                } else {
-                  newBoard[startRow + i][startCol] = ship;
-                }
-              }
+          for (let i = 0; i < ship.length; i++) {
+            if (orientation === "horizontal") {
+              newBoard[startRow][startCol + i] = ship;
+            } else {
+              newBoard[startRow + i][startCol] = ship;
             }
           }
         }
+      }
+    }
 
-        setBoardState(newBoard);
-      };
+    setBoardState(newBoard);
+    setIsInitialized(true);
+  };
 
+  useEffect(() => {
+    if (!isInitialized) {
       placeShipsRandomly();
     }
-  }, [boardState]);
+  }, [isInitialized]);
 
   return boardState;
 };
