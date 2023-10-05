5fd727a57bf4d363ee05187c0a1f43fbe9ad3f65
GrzegorzJendernal <g.jendernal@gmail.com>
2023-10-05 21:38:03 +0200

    redefined ships

--- src/utils/ships.ts
+++ src/utils/ships.ts
@@ -7,35 +7,46 @@ interface Ship {
   orientation: "horizontal" | "vertical";
 }
 
-export const singleMastedShip: Ship = {
-  name: "single-masted ship",
-  id: 1,
-  length: 1,
-  hit: false,
-  sunk: false,
-  orientation: "horizontal",
+interface Ship {
+  name: string;
+  id: number;
+  length: number;
+  hit: boolean;
+  sunk: boolean;
+  orientation: "horizontal" | "vertical";
+}
+
+export let ships: Ship[] = [];
+
+const shipTypes = [
+  { name: "single-masted ship", length: 1 },
+  { name: "two-masted ship", length: 2 },
+  { name: "three-masted ship", length: 3 },
+  { name: "four-masted ship", length: 4 },
+];
+
+const shipCounts: Record<string, number> = {
+  "single-masted ship": 4,
+  "two-masted ship": 3,
+  "three-masted ship": 2,
+  "four-masted ship": 1,
 };
-export const twoMastedShip: Ship = {
-  name: "two-masted ship",
-  id: 1,
-  length: 2,
-  hit: false,
-  sunk: false,
-  orientation: "horizontal",
-};
-export const threeMastedShip: Ship = {
-  name: "three-masted ship",
-  id: 1,
-  length: 3,
-  hit: false,
-  sunk: false,
-  orientation: "horizontal",
-};
-export const fourMastedShip: Ship = {
-  name: "four-masted ship",
-  id: 1,
-  length: 4,
-  hit: false,
-  sunk: false,
-  orientation: "horizontal",
-};
+
+let shipId = 1;
+
+const newShips: Ship[] = shipTypes.flatMap((shipType) => {
+  const { name, length } = shipType;
+  const count = shipCounts[name];
+
+  return Array.from({ length: count }, () => ({
+    name,
+    id: shipId++,
+    length,
+    hit: false,
+    sunk: false,
+    orientation: "horizontal",
+  }));
+});
+
+// Przypisujemy nową tablicę do zmiennej ships
+ships = newShips;
