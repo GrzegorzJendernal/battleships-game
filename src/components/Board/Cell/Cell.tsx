f286c551b8990ba5053a200ae3d6f4b5462cb08b
GrzegorzJendernal <g.jendernal@gmail.com>
2023-10-05 21:38:18 +0200

    added occupied prop

--- src/components/Board/Cell/Cell.tsx
+++ src/components/Board/Cell/Cell.tsx
@@ -1,10 +1,10 @@
 import { StyledCell } from "./cell.styled"
 
-const Cell = ({children, id, coordinate}: CellProps) => {
+const Cell = ({children, id, coordinate, occupied}: CellProps) => {
   console.log(id, coordinate);
 
   return (
-    <StyledCell id={id}>{children}</StyledCell>
+    <StyledCell id={id} occupied={occupied}>{children}</StyledCell>
   );
 };
 
