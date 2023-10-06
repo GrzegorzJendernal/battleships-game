71beeb19a1dc4b48dcdcac880989831aeafe176a
GrzegorzJendernal <g.jendernal@gmail.com>
2023-10-06 22:37:52 +0200

    deleted unused states

--- src/utils/labels.ts
+++ src/utils/labels.ts
@@ -1,7 +1,2 @@
 export const columns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 export const rows = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
-
-
-// const [isOccupied, setIsOccupied] = useState<boolean>(false);
-// const [isHit, setIsHit] = useState<boolean>(false);
-// const [isSunk, setIsSunk] = useState<boolean>(false);
