02562fa4a60044a8a1bfd29cda49955b4ad7ace7
GrzegorzJendernal <g.jendernal@gmail.com>
2023-10-10 22:01:47 +0200

    updated App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
+import { Container } from "../components/Container/Container";
 import EnemyBoard from "../components/EnemyBoard/EnemyBoard";
+import PlayerBoard from "../components/PlayerBoard/PlayerBoard";
 import { GlobalStyle } from "./GlobalStyle";
+import StartScreen from "../components/StartScreen/StartScreen";
 
 const App = () => {
+  const [gameState, setGameState] = useState<GameState>("start");
   return (
     <>
       <GlobalStyle />
       <div>Tu będzie zajebiaszcza gra w statki</div>
-      <EnemyBoard />
+      <Container>
+        {gameState === "start" && <StartScreen setState={setGameState}/>}
+        <PlayerBoard state={gameState} setState={setGameState}/>
+        {gameState === "game" && <EnemyBoard />}
+      </Container>
     </>
   );
 };
