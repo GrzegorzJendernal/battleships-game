663af0c4983890171be39c4650a81020d056a1cb
GrzegorzJendernal <g.jendernal@gmail.com>
2023-10-11 17:44:07 +0200

    added utilities to player Board

--- src/components/PlayerBoard/PlayerBoard.tsx
+++ src/components/PlayerBoard/PlayerBoard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useBoardState } from "../../utils/useBoardState";
 import Board from "../Board/Board";
 import ShipList from "../ShipList/ShipList";
@@ -9,13 +10,18 @@ const PlayerBoard = ({
   state: GameState;
   setState: React.Dispatch<React.SetStateAction<GameState>>;
 }) => {
-  const { boardState, shipsState, handleShot, handleShipPlacement } = useBoardState();
-
+  const player = true;
+  const { boardState, shipsState, handleShot, handleShipPlacement, reset, setIsRandom } = useBoardState(player);
+  const [playerShips, setPlayerShips] = useState(shipsState);
+  
   if (state === "preparation")
     return (
       <>
-        <Board boardState={boardState} shipsState={shipsState} handleClick={handleShipPlacement} />
-        <ShipList gameState={state} shipsState={shipsState} />
+        <Board boardState={boardState} shipsState={playerShips} handleClick={handleShipPlacement} />
+        <button onClick={() => reset()}>Clear Board</button>
+        <button onClick={() => setIsRandom(true)}>Place ships randomly</button>
+        <button onClick={() => setState("game")}>Start game</button>
+        <ShipList gameState={state} ships={playerShips} setShips={setPlayerShips}/>
       </>
     );
   if (state === "game") return <Board boardState={boardState} shipsState={shipsState} handleClick={handleShot} />;
