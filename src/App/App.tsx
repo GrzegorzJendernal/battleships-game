bec9b99b03cdcae6de26990c761747ccc8622b2f
GrzegorzJendernal <g.jendernal@gmail.com>
2023-10-11 21:49:45 +0200

    updated App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,17 +4,22 @@ import EnemyBoard from "../components/EnemyBoard/EnemyBoard";
 import PlayerBoard from "../components/PlayerBoard/PlayerBoard";
 import { GlobalStyle } from "./GlobalStyle";
 import StartScreen from "../components/StartScreen/StartScreen";
+import Result from "../components/Result/Result";
 
 const App = () => {
   const [gameState, setGameState] = useState<GameState>("start");
+  const [playerTurn, setPlayerTurn] = useState(true);
+  
   return (
     <>
       <GlobalStyle />
       <div>Tu będzie zajebiaszcza gra w statki</div>
       <Container>
         {gameState === "start" && <StartScreen setState={setGameState}/>}
-        <PlayerBoard state={gameState} setState={setGameState}/>
-        {gameState === "game" && <EnemyBoard />}
+        <PlayerBoard state={gameState} setState={setGameState} turn={playerTurn} setTurn={setPlayerTurn}/>
+        {gameState === "game" && <EnemyBoard state={gameState} setState={setGameState} setTurn={setPlayerTurn}/>}
+        {gameState === "enemyWin" && <Result state={gameState}/>}
+        {gameState === "playerWin" && <Result state={gameState}/>}
       </Container>
     </>
   );
