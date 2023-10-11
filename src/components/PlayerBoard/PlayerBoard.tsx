dac67da5abeec39b61a57c1f7fef6bdf835f295d
GrzegorzJendernal <g.jendernal@gmail.com>
2023-10-11 21:49:09 +0200

    updated playerBoard

--- src/components/PlayerBoard/PlayerBoard.tsx
+++ src/components/PlayerBoard/PlayerBoard.tsx
@@ -1,30 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useBoardState } from "../../utils/useBoardState";
 import Board from "../Board/Board";
 import ShipList from "../ShipList/ShipList";
+import { enemyIsShooting } from "../../utils/enemyIsShooting";
+import { allShipsAreSunk } from "../../utils/shipsHelpers";
 
 const PlayerBoard = ({
   state,
   setState,
-}: {
-  state: GameState;
-  setState: React.Dispatch<React.SetStateAction<GameState>>;
-}) => {
+  turn,
+  setTurn,
+}: PlayerBoardProps) => {
   const player = true;
-  const { boardState, shipsState, handleShot, handleShipPlacement, reset, setIsRandom } = useBoardState(player);
+  const { boardState, shipsState, handleShipPlacement, reset, setIsRandom, handleShot } = useBoardState(player);
   const [playerShips, setPlayerShips] = useState(shipsState);
-  
+
+  useEffect(() => {
+    if (!turn && state === "game") {
+      enemyIsShooting(boardState, handleShot, setTurn);
+    }
+  }, [turn, state]);
+
+  useEffect(() => {
+    if (allShipsAreSunk(shipsState)) {
+      setState("playerWin");
+    }
+  }, [shipsState]);
+
   if (state === "preparation")
     return (
       <>
-        <Board boardState={boardState} shipsState={playerShips} handleClick={handleShipPlacement} />
+        <Board boardState={boardState} shipsState={playerShips} handleClick={handleShipPlacement} player={player}/>
         <button onClick={() => reset()}>Clear Board</button>
         <button onClick={() => setIsRandom(true)}>Place ships randomly</button>
         <button onClick={() => setState("game")}>Start game</button>
-        <ShipList gameState={state} ships={playerShips} setShips={setPlayerShips}/>
+        <ShipList gameState={state} ships={playerShips} setShips={setPlayerShips} />
       </>
     );
-  if (state === "game") return <Board boardState={boardState} shipsState={shipsState} handleClick={handleShot} />;
+  if (state === "game")
+    return (
+      <>
+        <Board boardState={boardState} shipsState={shipsState} player={player}/>
+        <ShipList gameState={state} ships={playerShips} />
+      </>
+    );
 };
 
 export default PlayerBoard;
