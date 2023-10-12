1a0d755375639b62332674ac87fc586aa132e11c
GrzegorzJendernal <g.jendernal@gmail.com>
2023-10-12 23:47:10 +0200

    updated playerBoard board

--- src/components/PlayerBoard/PlayerBoard.tsx
+++ src/components/PlayerBoard/PlayerBoard.tsx
@@ -1,47 +1,35 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useBoardState } from "../../utils/useBoardState";
 import Board from "../Board/Board";
 import ShipList from "../ShipList/ShipList";
-import { enemyIsShooting } from "../../utils/enemyIsShooting";
-import { allShipsAreSunk } from "../../utils/shipsHelpers";
+import usePlayerBoardEffects from "../../utils/usePlayerBoardsEffects";
 
-const PlayerBoard = ({
-  state,
-  setState,
-  turn,
-  setTurn,
-}: PlayerBoardProps) => {
+const PlayerBoard = ({ state, setState, turn, setTurn }: PlayerBoardProps) => {
   const player = true;
-  const { boardState, shipsState, handleShipPlacement, reset, setIsRandom, handleShot } = useBoardState(player);
-  const [playerShips, setPlayerShips] = useState(shipsState);
-
-  useEffect(() => {
-    if (!turn && state === "game") {
-      enemyIsShooting(boardState, handleShot, setTurn);
-    }
-  }, [turn, state]);
-
-  useEffect(() => {
-    if (allShipsAreSunk(shipsState)) {
-      setState("playerWin");
-    }
-  }, [shipsState]);
+  const [enemyShotAgain, setEnemyShotAgain] = useState(false);
+  const { boardState, shipsState, setShipsState, handleShipPlacement, reset, setIsRandom, handleShot } = useBoardState(
+    setTurn,
+    player,
+    setEnemyShotAgain,
+  );
+  usePlayerBoardEffects(turn, state, boardState, shipsState, enemyShotAgain, setEnemyShotAgain, setState, handleShot);
+  
 
   if (state === "preparation")
     return (
       <>
-        <Board boardState={boardState} shipsState={playerShips} handleClick={handleShipPlacement} player={player}/>
+        <Board boardState={boardState} shipsState={shipsState} handleClick={handleShipPlacement} player={player} />
         <button onClick={() => reset()}>Clear Board</button>
         <button onClick={() => setIsRandom(true)}>Place ships randomly</button>
         <button onClick={() => setState("game")}>Start game</button>
-        <ShipList gameState={state} ships={playerShips} setShips={setPlayerShips} />
+        <ShipList gameState={state} ships={shipsState} setShips={setShipsState} />
       </>
     );
   if (state === "game")
     return (
       <>
-        <Board boardState={boardState} shipsState={shipsState} player={player}/>
-        <ShipList gameState={state} ships={playerShips} />
+        <Board boardState={boardState} shipsState={shipsState} player={player} />
+        <ShipList gameState={state} ships={shipsState} />
       </>
     );
 };
