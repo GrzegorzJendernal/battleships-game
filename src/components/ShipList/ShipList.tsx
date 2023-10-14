ff5cebb780329b4e57c3451879f72acc90a4bc92
GrzegorzJendernal <g.jendernal@gmail.com>
2023-10-14 02:19:03 +0200

    updated ShipList

--- src/components/ShipList/ShipList.tsx
+++ src/components/ShipList/ShipList.tsx
@@ -1,5 +1,5 @@
 import { selectShip, rotateShip } from "../../utils/shipsHelpers";
-import { List, ListItem, ShipName } from "./shipList.styled";
+import { List, ListBox, ListItem, ShipName, ShipNumber } from "./shipList.styled";
 
 const ShipList = ({
   ships,
@@ -9,29 +9,38 @@ const ShipList = ({
   ships: Ship[];
   gameState: GameState;
   setShips?: React.Dispatch<React.SetStateAction<Ship[]>>;
-}) => {
-  return (
-    <div>
-      <List>
-        {ships.map((ship) => (
-          <ListItem key={ship.id} $selected={ship.selected} $placed={gameState === "preparation" && ship.placed} $hit={!ship.sunk && ship.hits > 0} $sunk={ship.sunk}>
-            <ShipName>{ship.name}</ShipName> {gameState === "game" && `${ship.hits > 0 ? "hit" : "in full strength"}`}{" "}
-            {gameState === "preparation" && setShips && (
-              <>
-                <button onClick={() => selectShip(setShips, ships, ship.id)} disabled={ship.placed}>
-                  select ship
-                </button>
-                {ship.length > 1 && <button onClick={() => rotateShip(setShips, ships, ship.id)} disabled={ship.placed}>
+}) => (
+  <ListBox $game={gameState === "game"}>
+    <List>
+      {ships.map((ship) => (
+        <ListItem
+          key={ship.id}
+          $selected={ship.selected}
+          $placed={gameState === "preparation" && ship.placed}
+          $hit={!ship.sunk && ship.hits > 0}
+          $sunk={ship.sunk}
+          $game={gameState === "game"}
+        >
+          <ShipName $game={gameState === "game"}>{ship.name}</ShipName>
+          {gameState === "game" && <ShipNumber>{ship.length}</ShipNumber>}{" "}
+          {gameState === "game" && `${ship.hits > 0 ? "hit" : "whole"}`}{" "}
+          {gameState === "preparation" && setShips && (
+            <>
+              <button onClick={() => selectShip(setShips, ships, ship.id)} disabled={ship.placed}>
+                select ship
+              </button>
+              {ship.length > 1 && (
+                <button onClick={() => rotateShip(setShips, ships, ship.id)} disabled={ship.placed}>
                   rotate {ship.orientation === "horizontal" ? "verically" : "horizontally"}
-                </button>}
-              </>
-            )}{" "}
-            {ship.sunk ? "& sunk" : ""}
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
-};
+                </button>
+              )}
+            </>
+          )}{" "}
+          {ship.sunk ? "& sunk" : ""}
+        </ListItem>
+      ))}
+    </List>
+  </ListBox>
+);
 
 export default ShipList;
