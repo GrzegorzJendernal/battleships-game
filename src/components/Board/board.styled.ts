7fd3554fe3f88445cbf2a48eb9c268ce92485250
GrzegorzJendernal <g.jendernal@gmail.com>
2023-10-14 02:20:36 +0200

    added styles to Board

--- src/components/Board/board.styled.ts
+++ src/components/Board/board.styled.ts
@@ -1,10 +1,25 @@
-import { styled } from "styled-components";
+import { css, styled } from "styled-components";
 
-export const BoardContainer = styled.div`
+export const BoardContainer = styled.div<BoardContainerProps>`
   display: grid;
-  grid-template-columns: auto repeat(10, 40px); /* 10 kolumn */
-  grid-template-rows: auto repeat(10, 40px); /* 10 wierszy */
+  grid-template-columns: auto repeat(10, 40px);
+  grid-template-rows: auto repeat(10, 40px);
   width: max-content;
   margin: 20px auto;
   border: 2px solid #333;
+
+  ${({ $playerTurn }) =>
+    !$playerTurn &&
+    css`
+      filter: brightness(90%);
+    `}
+
+  @media (max-width: 767px) {
+    grid-template-columns: auto repeat(10, 20px);
+    grid-template-rows: auto repeat(10, 20px);
+  }
+`;
+
+export const Title = styled.h2`
+  text-align: center;
 `;
