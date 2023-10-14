ca6b6766c8fff406eb24c477e042e8f8ab156d46
GrzegorzJendernal <g.jendernal@gmail.com>
2023-10-14 02:20:00 +0200

    updated Cell

--- src/components/Board/Cell/Cell.tsx
+++ src/components/Board/Cell/Cell.tsx
@@ -6,17 +6,24 @@ export const Cell = styled.div<CellProps>`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: ${({ $hit, $sunk }) => {
+  background-color: ${({ $occupied, $hit, $sunk }) => {
     switch (true) {
+      case $occupied:
+        return `#478df7`
       case $hit:
-        return `#ccc`;
+        return `#eda70f`;
       case $sunk:
         return `#ff0a0a`;
       default:
         return `#fff`;
     }
   }};
-  border: 1px solid ${({ $occupied }) => ($occupied ? `#ff0000` : `#ccc`)};
+  border: 1px solid #ccc;
   font-size: 1rem;
   font-weight: bold;
+
+  @media (max-width: 767px) {
+    width: 20px;
+    height: 20px;
+  }
 `;
